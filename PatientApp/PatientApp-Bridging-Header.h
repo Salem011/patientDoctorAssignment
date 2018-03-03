fba3d9251b1ea260ccd9fcbc9ca7712e64e8d4f4
Salem Mohammed <salem.muhmed@gmail.com>
2018-03-03 19:16:51 +0200

    installing sdwebimage and mbprogresshud

---
+++ PatientApp/PatientApp-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PatientApp-Bridging-Header.h
+//  PatientApp
+//
+//  Created by Salem Mohamed on 3/3/18.
+//  Copyright © 2018 Salem Mohamed. All rights reserved.
+//
+
+#import <MBProgressHUD/MBProgressHUD.h>
+#import <SDWebImage/UIImageView+WebCache.h>
