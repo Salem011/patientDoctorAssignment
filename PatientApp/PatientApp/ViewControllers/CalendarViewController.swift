6a8c27e5ffd549338523f16daf9dd332533a3c08
Salem Mohammed <salem.muhmed@gmail.com>
2018-03-03 21:58:49 +0200

    add validation to the appointment date

--- PatientApp/PatientApp/ViewControllers/CalendarViewController.swift
+++ PatientApp/PatientApp/ViewControllers/CalendarViewController.swift
@@ -22,19 +22,26 @@ class CalendarViewController: UIViewController, FSCalendarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        formatter.dateFormat = "yyyy/MM/dd"
+        formatter.dateFormat = "dd-MM-yyyy"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - FSCalendar Delegate
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         
+        let result = Calendar.current.compare(date, to: Date(), toGranularity: .day)
+        if monthPosition == .previous || result == .orderedAscending {
+            displayErrorAlert()
+            return
+        }
         selectedDate = date
         displayConfirmationAlert(for: formatter.string(from: date))
     }
     
+    // MARK: - Helping Functions
     func displayConfirmationAlert (for date: String) {
         let alert = UIAlertController(title: "Confirm Reservation", message: "Are you sure you want to reserve an appointment at: \(date)?", preferredStyle: .alert)
         let yesAction = UIAlertAction(title: "Yes", style: .default, handler: {void in
@@ -46,6 +53,12 @@ class CalendarViewController: UIViewController, FSCalendarDelegate {
         })
         alert.addAction(yesAction)
         alert.addAction(noAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func displayErrorAlert() {
+        let alert = UIAlertController(title: "Error", message: "You can only select today's date and above.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
 
