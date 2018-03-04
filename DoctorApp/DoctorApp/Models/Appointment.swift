1c79aeaffa9990e213c2d7dc14ddc0fc8523351f
Salem Mohammed <salem.muhmed@gmail.com>
2018-03-04 07:20:05 +0200

    create appointment model

---
+++ DoctorApp/DoctorApp/Models/Appointment.swift
@@ -0,0 +1,34 @@
+//
+//  Appointment.swift
+//  DoctorApp
+//
+//  Created by Salem Mohamed on 3/4/18.
+//  Copyright © 2018 Salem Mohamed. All rights reserved.
+//
+
+import Foundation
+
+struct Appointment {
+    
+    var id: String
+    var doctorId: String
+    var patientId: String
+    var status: String
+    
+    init?(id: String, dictionary: [String : Any]) {
+        guard let doctorId = dictionary["doctorId"] as? String,
+            let patientId = dictionary["patientId"] as? String,
+        let status = dictionary["status"] as? String else {
+                return nil
+        }
+        
+        self.id = id
+        self.doctorId = doctorId
+        self.patientId = patientId
+        self.status = status
+    }
+    
+    
+    
+    
+}
