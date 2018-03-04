41c3e6475261ced429d7d5704224eb90c9aaaac0
Salem Mohammed <salem.muhmed@gmail.com>
2018-03-04 07:26:05 +0200

    create doctor app bridging header

---
+++ DoctorApp/DoctorApp-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  DoctorApp-Bridging-Header.h
+//  DoctorApp
+//
+//  Created by Salem Mohamed on 3/4/18.
+//  Copyright © 2018 Salem Mohamed. All rights reserved.
+//
+
+#import <MBProgressHUD/MBProgressHUD.h>
+
